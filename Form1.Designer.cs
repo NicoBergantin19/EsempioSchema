97fdb56cae6c62199d5d6609726039d9134a7d6b
Nicox19 <75247663+Nicox19@users.noreply.github.com>
2021-10-27 20:40:25 +0200

    Finito

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace EsempioSchema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.closeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(489, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            this.viewToolStripMenuItem.Click += new System.EventHandler(this.viewToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(144, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nome Tabella";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 256);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lista Tabelle";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace EsempioSchema
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public SqlConnection Conn = new SqlConnection("Data Source=DESKTOP-0JNBS50;Initial Catalog=Tabella;Integrated Security=True");
+        //public SqlConnection Conn = new SqlConnection(@"Data Source=INFORMATICA216\INFORMATICA225;Initial Catalog=Auto;Integrated Security=True");
+        public SqlCommand cmd;
+        public SqlDataReader reader;
+        public Tabella Tabella = new Tabella();
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Conn.Open();
+                MessageBox.Show("La connessione stata aperta", "", MessageBoxButtons.OK);
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show("Errore: " + error.ToString(), "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //per controllare se la tabella è stata creata 
+            string cont = @"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'LISTA';";
+            cmd = new SqlCommand(cont, Conn);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows == false)
+            {
+                reader.Close();
+                cmd.Cancel();
+                string creaz = @"CREATE TABLE LISTA(ID int IDENTITY(1, 1) NOT NULL, Nome_Tabella nvarchar(100) NOT NULL);";
+                cmd = new SqlCommand(creaz, Conn);
+                try
+                {
+                    cmd.ExecuteNonQuery();  //esegue il comando di creare la tabella, inserisce dati nella tabella
+                    MessageBox.Show("Tabella creata.", "", MessageBoxButtons.OK);
+                }
+                catch (SqlException error)
+                {
+                    MessageBox.Show("Mi spiace, hai commesso un errore, mi spiego meglio: " + error.ToString());
+                }
+            }
+        }
+        private void viewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Conn.State==ConnectionState.Closed)
+            {
+                Conn.Open();
+            }
+
+            cmd = new SqlCommand(@"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES", Conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<AAAAAAA> lista = new List<AAAAAAA>();
+            
+            while (reader.Read())
+            {
+                lista.Add(new AAAAAAA { Kokko = (string)reader["TABLE_NAME"] });
+                dataGridView1.Rows.Add((string)reader["TABLE_NAME"]);
+                Tabella.NomeTab = reader.ToString();  
+            }
+            reader.Close();
+            cmd.Cancel();
+            Inserimento(lista);
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Conn.State == ConnectionState.Closed)
+            {
+                MessageBox.Show("La connessione è già chiusa", "", MessageBoxButtons.OK);
+            }
+            else
+            {
+                Conn.Close();
+                MessageBox.Show("La connessione è stata chiusa correttamente", "", MessageBoxButtons.OK);
+            }
+        }
+        public void Inserimento(List<AAAAAAA> sussybaka)
+        {
+            string valori = String.Join(",", sussybaka);
+            
+            string query = $"INSERT INTO dbo.LISTA (Nome_Tabella)VALUES{valori};";
+            SqlCommand cmd = new SqlCommand(query, Conn);
+            if (Conn.State != ConnectionState.Open)
+            {
+                Conn.Open();
+            }
+            cmd.ExecuteNonQuery();
+            Conn.Close();
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+    public class Tabella
+    {
+        public string NomeTab { get; set; }    
+    }
+
+    public class AAAAAAA
+    {
+        public string Kokko { get; set; }
+        public override string ToString()
+        {
+            string jiji = $"('{Kokko}')";
+            return jiji;
+        }
+    }
+}
